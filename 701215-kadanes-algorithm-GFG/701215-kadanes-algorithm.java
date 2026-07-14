327a031ebe4e318dd083ef31d3ab56097fd69262
Pawan Yadav <pkyyadav8521@gmail.com>
2026-07-14 11:16:50 +0530

    Added solution

--- 701215-kadanes-algorithm-GFG/701215-kadanes-algorithm.java
+++ 701215-kadanes-algorithm-GFG/701215-kadanes-algorithm.java
@@ -4,24 +4,12 @@ class Solution {
         int n = arr.length;
         int sum = 0;
         int max = Integer.MIN_VALUE;
-        
-        
-    //     for(int i = 0 ; i < n; i++){
-    //         int sum = 0;
-    //         for(int j = i ; j < n ; j++){
-    //             sum += arr[j];
-    //             max = Math.max(sum,max);
-    // }
-    //     }
-        
-        for(int i = 0; i < n ; i++){
-          sum += arr[i];
-          max = Math.max(sum,max);
-          if(sum < 0){
-              sum = 0;
-          }
-        
-        
+        for(int i = 0 ;i < n ; i ++){
+             sum += arr[i];
+            max = Math.max(sum,max);
+            if(sum < 0){
+                sum = 0;
+            }
         }
         return max;
     }
