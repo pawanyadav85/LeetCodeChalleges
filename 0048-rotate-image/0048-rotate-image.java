8e82a8faf1a305130cf4b123b642ce19ecb770d4
Pawan Yadav <pkyyadav8521@gmail.com>
2026-07-22 21:11:43 +0530

    Time: 0 ms (100%), Space: 43.4 MB (94.03%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,18 +1,18 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        //transpose of given matrix
-        for(int i = 1 ; i < matrix.length; i++){
-            for(int j = 0 ; j < i; j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
+ 
+        // taranspose of given matrix 
+        for(int i = 0; i < matrix.length; i++){
+        for(int j = 0; j < i;j++){
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
         }
-        //reverse each row 
+        }
+        // Reverse of the Matrix
         for(int i = 0 ; i < matrix.length; i++){
         int lo = 0;
         int hi = matrix[0].length - 1;
-        
         while(lo < hi){
             int temp = matrix[i][lo];
             matrix[i][lo] = matrix[i][hi];
@@ -23,7 +23,3 @@ class Solution {
         }
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
