61a66af630dfe38fffee11ceafeb8a6609f40add
Pawan Yadav <pkyyadav8521@gmail.com>
2026-07-23 12:33:26 +0530

    Time: 1 ms (98.16%), Space: 47.8 MB (12.19%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,17 +1,17 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int[] row = new int [matrix.length];
-        int[] col = new int [matrix[0].length];
-        for(int i = 0 ;i < matrix.length; i++){
-            for(int j = 0 ;j < matrix[i].length;j++){
+        int[] row = new int[matrix.length];
+        int[] col = new int[matrix[0].length];
+        for(int i = 0 ; i < matrix.length; i ++){
+            for(int j = 0 ; j < matrix[0].length;j ++){
                 if(matrix[i][j] == 0){
                     row[i] = 1;
-                    col[j] = 1; 
+                    col[j] = 1;
                 }
             }
         }
-        for(int i = 0 ;i < matrix.length; i ++){
-            for(int j = 0 ; j < matrix[0].length; j++){
+        for(int i = 0 ; i < matrix.length; i++){
+            for(int j = 0 ; j < matrix[0].length;j++){
                 if(row[i] == 1 || col[j] == 1){
                     matrix[i][j] = 0;
                 }
@@ -19,7 +19,3 @@ class Solution {
         }
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
